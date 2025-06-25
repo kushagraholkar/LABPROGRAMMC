763517342e7c8e100f1b66ee24963886a7fe9b97
kushagraholkar <kinshuholkar22@gmail.com>
2025-06-25 13:47:06 +0530

    Create assignment.c

---
+++ assignment.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main(){
+		// Program 1
+//	printf("Meet Solanki\n\tIndore instute of science and technology\n\t\tIndore");
+
+
+   		// Program2 {Sum of two num}
+//   int a ,b;
+//   printf("Enter a ");
+//   scanf("%d",&a);
+//   printf("Enter b ");
+//   scanf("%d",&b);
+//   int ans = a + b;
+//   printf("Addition is -> %d ",ans);
+
+		// Program3 Average 
+
+//printf("Enter the marks of 4 subjects");
+//int s1,s2,s3,s4;
+//scanf("%d%d%d%d",&s1,&s2,&s3,&s4);
+//int total = s1+s2+s3+s4;
+//float avg = ( s1+s2+s3+s4)/4.0;
+//printf("Total makrs = %d\n",total);
+//printf("Average is = %f",avg);
+
+		// Program4  {get char}
+//		printf("Enter char ");
+//		char ch;
+//		scanf("%c",&ch);
+//		int ascai = (int) ch;
+//		printf("the character is %c & the ascai is %d",ch,ascai);
+//		
+
+			// Program5 {swap}
+			printf("Enter num1 & num2\n");
+			int num1,num2;
+			scanf("%d",&num1);
+			scanf("%d",&num2);
+			printf("Value of num1 before swap is %d & Value of num2 before swap is %d  \n",num1,num2);
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+			printf("Value of num1 After swap is %d & Value of num2 After swap is %d  \n",num1,num2);
+				
+
+}
